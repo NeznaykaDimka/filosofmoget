a9289f399d94b6b382e57c4f2ee3536599392612
NeznaykaDimka <94982726+NeznaykaDimka@users.noreply.github.com>
2025-10-17 20:34:10 +0300

    Add files via upload

---
+++ script-notebook.js
@@ -0,0 +1,5 @@
+var Welcome=prompt('добро пожаловать в записную книжку. здесь бардак, постарайтесь не обращать внимание');
+var Quotes=['aaaa', 'bbbbb', 'ccccc', 'dddddd', 'fffff', 'eeeee'];
+button onclick="myFunction"(Quotes){
+alert(Quotes);
+}
