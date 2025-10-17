79b64b221a5f0b1a4b0fbd5d5c3fd1889a8d88ee
NeznaykaDimka <94982726+NeznaykaDimka@users.noreply.github.com>
2025-10-17 20:35:49 +0300

    Delete script-notebook.js

--- script-notebook.js
+++
@@ -1,5 +0,0 @@
-var Welcome=prompt('добро пожаловать в записную книжку. здесь бардак, постарайтесь не обращать внимание');
-var Quotes=['aaaa', 'bbbbb', 'ccccc', 'dddddd', 'fffff', 'eeeee'];
-button onclick="myFunction"(Quotes){
-alert(Quotes);
-}
